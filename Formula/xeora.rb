c8574e7e8b9b727a117d71f7e5aedad5d9068f77
Tuna Celik <tuna@jakpark.com>
2021-09-30 09:17:56 +0200

    xeora v7.4.7942

--- Formula/xeora.rb
+++ Formula/xeora.rb
@@ -1,8 +1,8 @@
 class Xeora < Formula
   desc "Web Development Framework"
   homepage "https://xeora.org"
-  url "https://github.com/xeora/v7-framework/archive/v7.4.7925.tar.gz"
-  sha256 "cf13e42e6cf81163b637b2695c8c9de94ecf010ca517436ab88d7393d9f249a2"
+  url "https://github.com/xeora/v7-framework/archive/v7.4.7942.tar.gz"
+  sha256 "f46f415dcca357e71b7b965936db497fe5b6edbebc84c4ae73f01479ff9e2ffc"
   license "MIT"
   head "https://github.com/xeora/v7-framework.git"
 
@@ -13,7 +13,7 @@ class Xeora < Formula
            "--configuration", "Release",
            "--framework", "net#{Formula["dotnet"].version.major_minor}",
            "--output", "out",
-           "/p:AssemblyVersion=7.4.7925",
+           "/p:AssemblyVersion=7.4.7942",
            "src/Xeora.CLI/Xeora.CLI.csproj"
 
     libexec.install Dir["out/*"]
